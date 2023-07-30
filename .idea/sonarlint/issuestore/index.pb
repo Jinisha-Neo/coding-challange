af452ba1cdacf54d8d51605d07f32e717658803b
Jinisha-Neo <jinisha.gehlot@neosoftmail.com>
2023-07-30 16:50:54 +0530

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+K
+coding_challage/Shares.java,2\a\2aa4ed1c883d7332d427aa7530c1185ddfe0e3fb
---
+++ coding_challage/Shares.java
@@ -0,0 +1,31 @@
+package coding_challage;
+
+import java.util.Scanner;
+
+public class Shares {
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int days = s.nextInt();
+        int sharePrice[] = new int[days];
+        int maxProfit = 0;
+        for(int i = 0; i < days; i++)
+        {
+            sharePrice[i] = s.nextInt();
+        }
+        int j = 0;
+        for(int i = 1; i < days; i++)
+        {
+            if(sharePrice[i-1] > sharePrice[i])
+            {
+                j = i;
+            }
+            if(sharePrice[i-1] < sharePrice[i] && (i+1 == days || sharePrice[i] > sharePrice[i+1]))
+            {
+                maxProfit = maxProfit + (sharePrice[i] - sharePrice[j]);
+            }
+        }
+
+        System.out.println("max profit is "+maxProfit);
+    }
+}
